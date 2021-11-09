e74b74afcac1d3b00d5b2d6ec41fca1a78cd0447
Omar <dijkstra.e.w@mail.ru>
2021-11-09 16:06:42 +0300

    1

---
+++ build/CMakeFiles/f_letter.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libf_letter.a"
+)
---
+++ build/CMakeFiles/multi_compute_matrix.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/multi_compute_matrix.dir/src/find_letters.c.o"
+  "libmulti_compute_matrix.pdb"
+  "libmulti_compute_matrix.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/multi_compute_matrix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
