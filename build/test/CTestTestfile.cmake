a07fb4e7129f9b0080191474efbcee66ed35a4e6
Omar <dijkstra.e.w@mail.ru>
2021-11-09 23:29:56 +0300

    last5

--- build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/omar/try/test
-# Build directory: /home/omar/try/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(find_letters "/home/omar/try/build/test/find_letters_tests")
-set_tests_properties(find_letters PROPERTIES  _BACKTRACE_TRIPLES "/home/omar/try/test/CMakeLists.txt;17;add_test;/home/omar/try/test/CMakeLists.txt;0;")
